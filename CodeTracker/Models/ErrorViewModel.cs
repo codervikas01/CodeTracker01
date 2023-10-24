ffe7544cfe07d5e54bb5f6c4f4a8d57edadc10bd
vikas <vikas@VIKASCHAUHAN>
2023-10-25 02:34:46 +0530

    Add project files.

---
+++ CodeTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
